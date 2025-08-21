4732599734b09c5e58a24fb0d88eab6e0b988404
Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>
2025-08-21 19:35:51 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const handleLoadAllGoods = async () => {
+    try {
+      const apiGoods = await getAll();
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      setGoods(apiGoods);
+    } catch (error) {
+      return;
+    }
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const handleLoadFirst5Goods = async () => {
+    try {
+      const apiGoods = await get5First();
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      setGoods(apiGoods);
+    } catch (error) {
+      return;
+    }
+  };
+
+  const handleLoadRedGoods = async () => {
+    try {
+      const apiGoods = await getRedGoods();
+
+      setGoods(apiGoods);
+    } catch (error) {
+      return;
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={handleLoadAllGoods}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={handleLoadFirst5Goods}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={handleLoadRedGoods}>
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
--- src/api/goods.ts
+++ src/api/goods.ts
@@ -3,14 +3,22 @@ import { Good } from '../types/Good';
 // eslint-disable-next-line
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
+const COLOR_RED = 'red';
+
 export function getAll(): Promise<Good[]> {
   return fetch(API_URL).then(response => response.json());
 }
 
 export const get5First = () => {
-  return getAll().then(goods => goods); // sort and get the first 5
+  return getAll().then(goods => {
+    goods.sort((a, b) => a.name.localeCompare(b.name));
+
+    return goods.slice(0, 5);
+  });
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods); // get only red
+  return getAll().then(goods => {
+    return goods.filter(good => good.color === COLOR_RED);
+  });
 };
